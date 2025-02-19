a8d9021208355df7490370d0446f46e4cfc89b42
Bernhard Thoben <berni-the-bear@web.de>
2025-02-19 19:00:09 +0100

    sensorservice: Maximum range of the APDS-9910 proximity sensor adjusted.
    Change-Id: Iaae9c7ecac9c2f2491f20202fa1b3b863c331da3

--- services/sensorservice/SensorDevice.cpp
+++ services/sensorservice/SensorDevice.cpp
@@ -115,6 +115,12 @@ void SensorDevice::initializeSensorList() {
                 // Don't crash here or the device will go into a crashloop.
                 ALOGW("%s should have a non-zero resolution", sensor.name);
             }
+
+            // DIM
+            if (strstr(sensor.name, "APDS-9910 Proximity")) {
+                sensor.maxRange = 4;
+                ALOGW("-DIM- %s's max range set to %.12f", sensor.name, sensor.maxRange);
+            }
         }
 
         // Check and clamp power if it is 0 (or close)
