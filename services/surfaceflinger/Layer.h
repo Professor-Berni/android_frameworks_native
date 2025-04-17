cbe4e664fcfb1b0bae615aa0eba317d9da7e7571
Kevin F. Haggerty <haggertk@lineageos.org>
2025-04-17 06:27:24 -0600

    Merge tag 'android-security-14.0.0_r19' into staging/lineage-21.0_android-security-14.0.0_r19
    Android security 14.0.0 release 19
    
    * tag 'android-security-14.0.0_r19':
      [DO NOT MERGE] Send dim layer in input list
      Ensure objects remain valid after calling policy
    
    Conflicts:
    	services/inputflinger/dispatcher/InputDispatcher.cpp
    	services/inputflinger/dispatcher/InputDispatcher.h
    
    Change-Id: I9260f3088b900b569d2d670d960250aa2d040842

--- services/surfaceflinger/Layer.h
+++ services/surfaceflinger/Layer.h
@@ -524,7 +524,7 @@ public:
      * the InputDispatcher to do PID based occlusion detection.
      */
     bool needsInputInfo() const {
-        return (hasInputInfo() || hasBufferOrSidebandStream()) && !mPotentialCursor;
+        return (hasInputInfo() || hasBufferOrSidebandStream() || fillsColor()) && !mPotentialCursor;
     }
 
     // Implements RefBase.
